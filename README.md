8978655cf52cd08ec03a6c1498fb611028fbbfb7
Antonio Stanziola <stanziola.antonio@gmail.com>
2021-04-11 00:06:35 +0100

    ode example

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 Linear uncertainty propagation using JAX transformations 
 
-## Example
+## Examples
+
+
+### Non linear function
 
 ```python
 from luprox import linear_uncertainty
@@ -23,5 +26,40 @@ Results:
 ```
 STD: 0.04352208226919174 (Linear) | 0.04308144003152847 (MC)
 ```
+![Example on a function](docs/example_function.jpg)
 
-![Example on a function](docs/example.jpg)
+### Damped pendulum simulation
+```python
+# Integration interval
+t = jnp.arange(0., 6.3, 0.01)
+
+# Damped pendulum simulation with uncertainty
+@linear_uncertainty
+def solve_pendulum(params):
+    g = params[0]
+    L = params[1]
+    alpha = params[2]
+    u0 = params[3:]
+    
+    def damped_pendulum(u, t):
+        theta = u[0]
+        vel = u[1]
+        d_theta = u[1]
+        d_vel = - (g/L)*jnp.sin(u[0]) - alpha*u[1]
+        return jnp.stack([d_theta, d_vel])
+    
+    trajectory = euler_integration(damped_pendulum, u0, dt, t)
+    return trajectory[:,0]
+
+# Parameters and covariances
+params = jnp.array([9.81, 1., .0, jnp.pi/3, 0.])
+cov_in = jnp.diag(jnp.array([0.0001, 0.001, 0.001, 0.002, 0.0001]))
+
+# Simulate damped pendulum with linear uncertainty propagation
+linear_trajectory, covariance = solve_linear(params, cov_in) 
+```
+
+![Example of uncertainty propagation on a damped pendulum simulation](docs/example_pendulum.jpg)
+
+# References
+The code in this repository is inspired by [`Measurements.jl`](https://github.com/JuliaPhysics/Measurements.jl) [[Giordano, 2016](https://arxiv.org/abs/1610.08716)] and the [Uncertainty programming lecture notes](https://mitmath.github.io/18337/lecture19/uncertainty_programming) by Chris Rackauckas.
--- luprox/interpreters.py
+++ luprox/interpreters.py
@@ -2,21 +2,22 @@ from typing import Any, Callable, Dict
 from jax import numpy as jnp
 from jax import jacfwd, jacrev, vmap, jit, random
 
-
 def linear_uncertainty(fun: Callable):
     """See Measurements.jl in https://mitmath.github.io/18337/lecture19/uncertainty_programming"""
 
-    def fun_with_uncertainty(mean, covariance):
+    def fun_with_uncertainty(mean, covariance, *args, **kwargs):
         mean = mean.real
         covariance = covariance.real
+        
+        def f(x): return fun(x, *args, **kwargs)
 
         # Getting output meand and covariance
-        out_mean = fun(mean)
+        out_mean = f(mean)
 
         if out_mean.size < mean.size:
-            J = jacrev(fun)(mean)
+            J = jacrev(f)(mean)
         else:
-            J = jacfwd(fun)(mean)
+            J = jacfwd(f)(mean)
 
         out_cov = jnp.matmul(J, jnp.matmul(covariance,J.T)) # this factor of 4 is odd
         return out_mean, out_cov
@@ -26,19 +27,19 @@ def linear_uncertainty(fun: Callable):
 
 def mc_uncertainty(fun: Callable, trials):
 
-    def _sample(mean, L, key):
-        noisy_x = mean + jnp.dot(L, random.normal(key, mean.shape))
-        return fun(noisy_x)
-
-    get_samples = vmap(_sample, in_axes=(None, None, 0))
-
     def fun_with_uncertainty(mean, covariance, key):
+
+        def _sample(mean, L, key, *args, **kwargs):
+            noisy_x = mean + jnp.dot(L, random.normal(key, mean.shape))
+            return fun(noisy_x, *args, **kwargs)
+
         mean = mean.real
         covariance = covariance.real
         keys = random.split(key, trials)
 
         L = jnp.linalg.cholesky(covariance)
-        samples = get_samples(mean, L, keys)
+        samples = vmap(_sample, in_axes=(None, None, 0))(mean, L, keys)
+
         return jnp.mean(samples, axis=0), jnp.cov(samples.T)
 
     return jit(fun_with_uncertainty)
